ea24d45e24b129c83322923ee1f2f1f334f9aae2
Rick McKenny <11lrm1@queensu.ca>
2020-03-17 14:57:54 -0400

    Added a subtract func and updated multiply

--- Calculator.py
+++ Calculator.py
@@ -1,7 +1,10 @@
 def add(a, b):
     return a+b
 def mult(a,b):
-    pass
+    return a*b
 def divide(a,b):
     #integer division
     return a//b
+def subtraction(a,b):
+    pass
+
--- Main.py
+++ Main.py
@@ -1,5 +1,7 @@
 import Calculator as calc
 def main():
     print(calc.add(5,5))
+    print(calc.mult(5,5))
+    print(calc.divide(5,5))
 
 main()
